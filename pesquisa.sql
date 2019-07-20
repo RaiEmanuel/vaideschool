6b6bbc68295e3f81af0b8dd307c584df5c0c50b8
RaiEmanuel <rai_emanuel@hotmail.com>
2019-07-20 20:32:02 -0300

    Add files via upload
    atualização na query de criação do banco. Exclusão dos ids de aluno e professor para a substituição para uma matrícula auto_increment em ambas as tabelas. Mudança de hora_aula default para 1h (tabela turma)

--- pesquisa.sql
+++ pesquisa.sql
@@ -1,13 +1,12 @@
 create table aluno(
-	idAluno bigserial primary key,
-	nome varchar(150) not null not null,
-	rg varchar(15) not null not null,
-	matricula varchar(15) not null not null,
-	senha varchar(64) not null not null
+	matricula bigserial primary key,
+	nome varchar(150) not null,
+	rg varchar(15) not null,
+	senha varchar(64) not null
 );
 
 create table professor(
-	idProfessor bigserial primary key,
+	matricula bigserial primary key,
 	nome varchar(150) not null,
 	rua varchar(100),
 	numero smallint,
@@ -18,24 +17,32 @@ create table professor(
 create table disciplina(
 	idDisciplina bigserial primary key,
 	nome varchar(50) not null,
-	carga_horaria smallint not null default 60,
-	hora_aula float not null default 0.91,
+	carga_horaria smallint not null default 60,--60h
+	hora_aula float not null default 1,--1h a dura√ß√£o da aula
 	check (carga_horaria > 0 and hora_aula > 0)
 );
 
-create type statusTurma as enum ('A','F');
+
+
+create type statusTurma as enum ('A','F');--aberta e fechada
 
 create table turma(
 	idTurma bigserial primary key,
 	local_aula varchar(50) not null,
 	status statusTurma not null default 'A',
-	professor_idProfessor bigserial references professor(idProfessor) on delete set null on update cascade,
+	professor_matriculaProfessor bigserial references professor(matricula) on delete set null on update cascade,
 	disciplina_idDisciplina bigserial references disciplina(idDisciplina) on delete restrict on update cascade
+);
+
+create table horario(
+	idHorario bigserial primary key,
+	horario varchar(6),
+	turma_idTurma bigserial references turma(idTurma) on delete cascade on update cascade
 );
 
 create table turma_has_aluno(
 	turma_idTurma bigserial references turma(idTurma) on delete restrict on update cascade,
-	aluno_idAluno bigserial references aluno(idAluno) on delete cascade on update cascade,
+	aluno_matriculaAluno bigserial references aluno(matricula) on delete cascade on update cascade,
 	p1 double precision not null default 0,
 	p2 double precision not null default 0,
 	p3 double precision not null default 0,
@@ -45,17 +52,10 @@ create table turma_has_aluno(
 	media_final double precision not null default 0
 );
 
-
-create table horario(
-	idHorario bigserial primary key,
-	horario varchar(6),
-	turma_idTurma bigserial references turma(idTurma) on delete cascade on update cascade
-);
-
 -- inserts
 
 insert into aluno values(default,'J','70495748623','20141084010110','Zema123');
-insert into aluno values (default,'BorÛ','70495748623','20151084010110','Boro123');
+insert into aluno values (default,'Bor√≥','70495748623','20151084010110','Boro123');
 
 insert into professor values(default,'Silvio','JACINTO P',42,'LAgoa Seca','Silvio123');
 insert into professor values(default,'PC','Augusta',69,'Centro','PC123');
@@ -75,10 +75,10 @@ insert into turma_has_aluno values(2,2,70,70,50,default,4,default,default);
 -- fim dos inserts
 
 
--- visıes
+-- vis√µes
 
-create view visao_alunos as (select d.idDisciplina as CÛdigo,a.nome as Nome,d.nome as Disciplina,
-							 tha.p1 as Unidade1,tha.p2 as Unidade2,tha.p3 as Unidade3,tha.p4 as Recuperac„o
+create view visao_alunos as (select d.idDisciplina as C√≥digo,a.nome as Nome,d.nome as Disciplina,
+							 tha.p1 as Unidade1,tha.p2 as Unidade2,tha.p3 as Unidade3,tha.p4 as Recuperac√£o
 							 ,tha.media_final as resultado,tha.faltas as faltas
 							 from aluno as a,disciplina as d, turma_has_aluno as tha, turma as t 
 							 where a.idaluno = tha.aluno_idAluno and tha.turma_idturma = t.idturma
@@ -86,4 +86,5 @@ create view visao_alunos as (select d.idDisciplina as CÛdigo,a.nome as Nome,d.no
 							 
 							 
 							 
---	fim das visıes						 
+--	fim das vis√µes		
+
