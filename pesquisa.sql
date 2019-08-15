09fe7ce9ae10aa94e4f2b4539ba7771c76a91254
RaiEmanuel <rai_emanuel@hotmail.com>
2019-08-15 05:30:25 -0300

    Add files via upload
    adição das funções e tabela ex aluno

--- pesquisa.sql
+++ pesquisa.sql
@@ -55,7 +55,7 @@ create table disciplina(
 	idDisciplina bigserial primary key,
 	nome varchar(50) not null unique,
 	carga_horaria smallint not null default 60,--60h
-	hora_aula float not null default 1,--1h a dura√ß√£o da aula
+	hora_aula float not null default 1,--1h a duraÁ„o da aula
 	check (carga_horaria > 0 and hora_aula > 0 and carga_horaria >= hora_aula)
 );
 
@@ -88,10 +88,25 @@ create table turma_has_aluno(
 	primary key(turma_idTurma, aluno_matriculaAluno)
 );
 
+create table ex_aluno(
+	matricula bigserial primary key,
+	nome varchar(150) not null unique,
+	rg varchar(15) not null,
+	senha varchar(64) not null,
+	rua varchar(100),
+	numero smallint,
+	bairro varchar(40),
+	data_exclusao date
+);
+
+create or replace rule adiciona_ex_aluno as
+on delete to aluno do insert into ex_aluno 
+values (old.matricula,old.nome,old.rg,old.senha,old.rua,old.numero,old.bairro,now());
+
 -- inserts
 
 insert into aluno values(default,'J','70495748623','20141084010110','Zema123');
-insert into aluno values (default,'Bor√≥','70495748623','20151084010110','Boro123');
+insert into aluno values (default,'BorÛ','70495748623','20151084010110','Boro123');
 
 insert into professor values(default,'Silvio','JACINTO P',42,'LAgoa Seca','Silvio123');
 insert into professor values(default,'PC','Augusta',69,'Centro','PC123');
@@ -119,10 +134,10 @@ insert into turma_has_aluno values(2,2,70,70,50,default,4,default,default);
 -- fim dos inserts
 
 
--- vis√µes
+-- visıes
 
-create view visao_alunos as (select d.idDisciplina as C√≥digo,a.nome as Nome,d.nome as Disciplina,
-							 tha.p1 as Unidade1,tha.p2 as Unidade2,tha.p3 as Unidade3,tha.p4 as Recuperac√£o
+create view visao_alunos as (select d.idDisciplina as CÛdigo,a.nome as Nome,d.nome as Disciplina,
+							 tha.p1 as Unidade1,tha.p2 as Unidade2,tha.p3 as Unidade3,tha.p4 as Recuperac„o
 							 ,tha.media_final as resultado,tha.faltas as faltas
 							 from aluno as a,disciplina as d, turma_has_aluno as tha, turma as t 
 							 where a.idaluno = tha.aluno_idAluno and tha.turma_idturma = t.idturma
@@ -137,28 +152,48 @@ create view home_aluno as (select d.nome as disciplina_nome ,t.local_aula as loc
 							 
 select * from home_aluno;
 
---	fim das vis√µes	
+--	fim das visıes	
+
+/* funÁıes
+
+
+create function calcula_media_final(float,float,float,float)
+
+-- p1,p2,p3,p4
+returns float
+as 
+'SELECT CASE
+	
+	WHEN $4 <= 0 THEN CAST(CAST((($1 + $2 + $3)/3) as numeric(3,1)) as float)
+	
+WHEN $4 > 0 THEN CAST(CAST(((($1 + $2 + $3)/3*6 + ($4)*4)/10) as numeric(3,1)) as float)
+
+END '
+
+language 'sql';
 
 
 
+create function pega_qtd_maxima_falta(bigint)
+-- id da turma
+returns float
+as 'select (0.25 * (d.carga_horaria /d.hora_aula)) from turma_has_aluno as tha,turma as t, disciplina as d 
+where tha.turma_idTurma = t.idturma and t.disciplina_idDisciplina = d.idDisciplina and tha.turma_idTurma = $1;'
+language 'sql';
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+create function teste_aprovado(float,float,float,float)
+--(media_final,p4,faltas_obtidas,faltas_maximas)
+returns boolean
+as 'SELECT CASE 
+		WHEN ($3 > $4) THEN false
+		WHEN (($2 > 0) and  ($1 < 5)) THEN false
+		WHEN (($2 <= 0) and ($1 < 7)) THEN false
+		ELSE true
+	END;	
+	'
+language 'sql';	
 
 
 
